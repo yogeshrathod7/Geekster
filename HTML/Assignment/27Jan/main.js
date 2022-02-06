69bd3caca92885c217402c9fd994bf5fb9a0295a
yogeshrathod7 <48018485+yogeshrathod7@users.noreply.github.com>
2022-02-06 21:20:48 +0530

    Add files via upload

---
+++ HTML/Assignment/27Jan/main.js
@@ -0,0 +1,18 @@
+// window.addEventListener('load',() =>{
+//     const form =document.querySelector("#new-task-form");
+//     const input=document.querySelector("#new-form-input");
+//     const list_el =document.querySelector("#tasks");
+//     form.addEventListener("sumit",(e)=>{
+//         e.preventDefault();
+
+//         const task =input.value;
+
+//         if (!task){
+//             alert("Please fill out the task");
+//         }
+//         else {
+//             console.log("success");
+//         }
+//     })
+// })
+ 
